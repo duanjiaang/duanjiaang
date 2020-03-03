548f43375a7dccf9dc33b9030f08c0a8f43fc0cd
duanjiaang <3044297850@qq.com>
2020-03-04 00:25:42 +0800

    修改

--- web01/javascript/duanjiaang.js
+++ web01/javascript/duanjiaang.js
@@ -1,9 +1,4 @@
-document.getElementById('1').addEventListener('click',function () {
-  document.getElementById('11').innerHTML='段家昂';
-}) 
-document.getElementById('2').addEventListener('click',function () {
-  document.getElementById('22').innerHTML='男';
-}) 
-document.getElementById('3').addEventListener('click',function () {
-  document.getElementById('33').innerHTML='18627640718';
-}) 
+document.write('<h1>姓名:段家昂 </h1>');
+document.write('<h1>性别：男</h1>');
+document.write('<h1>电话：18627640718</h1>');
+
