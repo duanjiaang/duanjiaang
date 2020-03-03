e726042d9d6f67feebd2b446c03183799264b183
duanjiaang <3044297850@qq.com>
2020-03-03 23:52:06 +0800

    xiugai

---
+++ web01/javascript/duanjiaang.js
@@ -0,0 +1,9 @@
+document.getElementById('1').addEventListener('click',function () {
+  document.getElementById('11').innerHTML='段家昂';
+}) 
+document.getElementById('2').addEventListener('click',function () {
+  document.getElementById('22').innerHTML='男';
+}) 
+document.getElementById('3').addEventListener('click',function () {
+  document.getElementById('33').innerHTML='18627640718';
+}) 
