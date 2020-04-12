b4ce3593d63fe7544098f58884837c528dff5d76
duanjiaang <3044297850@qq.com>
2020-04-12 17:06:07 +0800

    修改

--- web01/js/demo0001.js
+++ web01/js/demo0001.js
@@ -2,10 +2,10 @@ console.log('进入demo0001.js');
 
 var myimg = document.getElementById('myimg');
 // js里面路径是由引用它的html位置决定
-myimg.setAttribute('src', './images/img12.jpg');
+myimg.setAttribute('src', './images/12.jpg');
 
 setTimeout(function () {
-  myimg.setAttribute('src', './images/img11.jpg');
+  myimg.setAttribute('src', './images/11.jpg');
 }, 5000);
 
 // 1 i L l I 0 o O
