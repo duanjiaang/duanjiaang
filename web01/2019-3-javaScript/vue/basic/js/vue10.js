e4c8a6692f115cddd5f4f2c4c4d5bab2c12f1642
duanjiaang <3044297850@qq.com>
2020-11-02 22:31:05 +0800

    vue和ajax数据联动

--- web01/2019-3-javaScript/vue/basic/js/vue10.js
+++ web01/2019-3-javaScript/vue/basic/js/vue10.js
@@ -9,7 +9,13 @@ let app = new Vue({
     error: {
       name: false,
       pwd: false
-    }
+    },
+    success: false,
+    list: [
+      { id: 100, name: '绿甲龙', sex: 'm' },
+      { id: 101, name: '吕佳龙', sex: 'm' },
+      { id: 102, name: '沈娇', sex: 'f' }
+    ]
   },
   methods: {
     login: function () {
@@ -17,10 +23,16 @@ let app = new Vue({
       // 重置错误
       this.error.name = false;
       this.error.pwd = false;
+      this.success = false;
       if (this.user.name.trim() == '') {
         this.error.name = true;
         return;
       }
+      if (this.user.pwd.trim() == '') {
+        this.error.pwd = true;
+        return;
+      }
+      this.success = true;
     }
   }
 });
